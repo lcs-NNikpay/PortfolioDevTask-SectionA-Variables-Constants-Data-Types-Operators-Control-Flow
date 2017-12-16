6ed4e46ababc4eeda36c992368c4154af119ecfc
Nicki Nikpay <nnikpay@lcs-mba3214.lcs.on.ca>
2017-12-16 11:01:50 -0500

    edited.

--- Portfolio Development Task.playground/Pages/Question 1.xcplaygroundpage/Contents.swift
+++ Portfolio Development Task.playground/Pages/Question 1.xcplaygroundpage/Contents.swift
@@ -11,8 +11,8 @@
  */
 // Answer question 1 below
 var howLongWeTravelledFor = 5.875
-var s = 80.0
-let dct = s * howLongWeTravelledFor
+var speedOfTheCar = 80.0
+let dct = speedOfTheCar * howLongWeTravelledFor
 
 /*:
 - callout(Shortcut): The **Edit All in Scope** menu command is useful when renaming variables – place your cursor within the variable name and then invoke the menu command.
